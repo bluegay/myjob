faa9fe8de7f77c88a3e68633877713cd206f8edb
jet <703179763@qq.com>
2014-06-21 17:20:57 +0800

    添加 ProRob 项目

---
+++ ProRob/source/ProRob/ProRob/Pro.Designer.cs
@@ -0,0 +1,248 @@
+﻿namespace ProRob
+{
+    partial class Pro
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.tab_help = new DevComponents.DotNetBar.TabItem(this.components);
+            this.tab_log = new DevComponents.DotNetBar.TabItem(this.components);
+            this.superTabControl1 = new DevComponents.DotNetBar.SuperTabControl();
+            this.superTabItem1 = new DevComponents.DotNetBar.SuperTabItem();
+            this.superTabControlPanel1 = new DevComponents.DotNetBar.SuperTabControlPanel();
+            this.list_log = new System.Windows.Forms.ListBox();
+            this.groupPanel1 = new DevComponents.DotNetBar.Controls.GroupPanel();
+            this.bt_start = new DevComponents.DotNetBar.ButtonX();
+            this.bt_opensource = new DevComponents.DotNetBar.ButtonX();
+            this.tb_source = new DevComponents.DotNetBar.Controls.TextBoxX();
+            this.labelX1 = new DevComponents.DotNetBar.LabelX();
+            ((System.ComponentModel.ISupportInitialize)(this.superTabControl1)).BeginInit();
+            this.superTabControl1.SuspendLayout();
+            this.superTabControlPanel1.SuspendLayout();
+            this.groupPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tab_help
+            // 
+            this.tab_help.Name = "tab_help";
+            this.tab_help.Text = "使用说明";
+            // 
+            // tab_log
+            // 
+            this.tab_log.Name = "tab_log";
+            this.tab_log.Text = "日志信息";
+            // 
+            // superTabControl1
+            // 
+            this.superTabControl1.AntiAlias = true;
+            // 
+            // 
+            // 
+            // 
+            // 
+            // 
+            this.superTabControl1.ControlBox.CloseBox.Name = "";
+            // 
+            // 
+            // 
+            this.superTabControl1.ControlBox.MenuBox.Name = "";
+            this.superTabControl1.ControlBox.Name = "";
+            this.superTabControl1.ControlBox.SubItems.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.superTabControl1.ControlBox.MenuBox,
+            this.superTabControl1.ControlBox.CloseBox});
+            this.superTabControl1.Controls.Add(this.superTabControlPanel1);
+            this.superTabControl1.Location = new System.Drawing.Point(26, 73);
+            this.superTabControl1.Name = "superTabControl1";
+            this.superTabControl1.ReorderTabsEnabled = true;
+            this.superTabControl1.SelectedTabFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Bold);
+            this.superTabControl1.SelectedTabIndex = 0;
+            this.superTabControl1.Size = new System.Drawing.Size(689, 387);
+            this.superTabControl1.TabFont = new System.Drawing.Font("宋体", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.superTabControl1.TabIndex = 8;
+            this.superTabControl1.Tabs.AddRange(new DevComponents.DotNetBar.BaseItem[] {
+            this.superTabItem1});
+            this.superTabControl1.Text = "superTabControl1";
+            // 
+            // superTabItem1
+            // 
+            this.superTabItem1.AttachedControl = this.superTabControlPanel1;
+            this.superTabItem1.GlobalItem = false;
+            this.superTabItem1.Name = "superTabItem1";
+            this.superTabItem1.Text = "日志信息";
+            // 
+            // superTabControlPanel1
+            // 
+            this.superTabControlPanel1.Controls.Add(this.list_log);
+            this.superTabControlPanel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.superTabControlPanel1.Location = new System.Drawing.Point(0, 28);
+            this.superTabControlPanel1.Name = "superTabControlPanel1";
+            this.superTabControlPanel1.Size = new System.Drawing.Size(689, 359);
+            this.superTabControlPanel1.TabIndex = 1;
+            this.superTabControlPanel1.TabItem = this.superTabItem1;
+            // 
+            // list_log
+            // 
+            this.list_log.BackColor = System.Drawing.SystemColors.Menu;
+            this.list_log.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.list_log.FormattingEnabled = true;
+            this.list_log.ItemHeight = 12;
+            this.list_log.Location = new System.Drawing.Point(0, 0);
+            this.list_log.Name = "list_log";
+            this.list_log.Size = new System.Drawing.Size(689, 359);
+            this.list_log.TabIndex = 1;
+            // 
+            // groupPanel1
+            // 
+            this.groupPanel1.CanvasColor = System.Drawing.SystemColors.Control;
+            this.groupPanel1.ColorSchemeStyle = DevComponents.DotNetBar.eDotNetBarStyle.Office2007;
+            this.groupPanel1.Controls.Add(this.bt_start);
+            this.groupPanel1.Controls.Add(this.bt_opensource);
+            this.groupPanel1.Controls.Add(this.tb_source);
+            this.groupPanel1.Controls.Add(this.labelX1);
+            this.groupPanel1.Location = new System.Drawing.Point(26, 12);
+            this.groupPanel1.Name = "groupPanel1";
+            this.groupPanel1.Size = new System.Drawing.Size(689, 55);
+            // 
+            // 
+            // 
+            this.groupPanel1.Style.BackColor2SchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground2;
+            this.groupPanel1.Style.BackColorGradientAngle = 90;
+            this.groupPanel1.Style.BackColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBackground;
+            this.groupPanel1.Style.BorderBottom = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderBottomWidth = 1;
+            this.groupPanel1.Style.BorderColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelBorder;
+            this.groupPanel1.Style.BorderLeft = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderLeftWidth = 1;
+            this.groupPanel1.Style.BorderRight = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderRightWidth = 1;
+            this.groupPanel1.Style.BorderTop = DevComponents.DotNetBar.eStyleBorderType.Solid;
+            this.groupPanel1.Style.BorderTopWidth = 1;
+            this.groupPanel1.Style.Class = "";
+            this.groupPanel1.Style.CornerDiameter = 4;
+            this.groupPanel1.Style.CornerType = DevComponents.DotNetBar.eCornerType.Rounded;
+            this.groupPanel1.Style.TextAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Center;
+            this.groupPanel1.Style.TextColorSchemePart = DevComponents.DotNetBar.eColorSchemePart.PanelText;
+            this.groupPanel1.Style.TextLineAlignment = DevComponents.DotNetBar.eStyleTextAlignment.Near;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseDown.Class = "";
+            this.groupPanel1.StyleMouseDown.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            // 
+            // 
+            // 
+            this.groupPanel1.StyleMouseOver.Class = "";
+            this.groupPanel1.StyleMouseOver.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.groupPanel1.TabIndex = 9;
+            // 
+            // bt_start
+            // 
+            this.bt_start.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.bt_start.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.bt_start.Location = new System.Drawing.Point(576, 12);
+            this.bt_start.Name = "bt_start";
+            this.bt_start.Size = new System.Drawing.Size(75, 23);
+            this.bt_start.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bt_start.TabIndex = 10;
+            this.bt_start.Text = "开始执行";
+            this.bt_start.Click += new System.EventHandler(this.bt_start_Click);
+            // 
+            // bt_opensource
+            // 
+            this.bt_opensource.AccessibleRole = System.Windows.Forms.AccessibleRole.PushButton;
+            this.bt_opensource.ColorTable = DevComponents.DotNetBar.eButtonColor.OrangeWithBackground;
+            this.bt_opensource.Location = new System.Drawing.Point(520, 12);
+            this.bt_opensource.Name = "bt_opensource";
+            this.bt_opensource.Size = new System.Drawing.Size(50, 23);
+            this.bt_opensource.Style = DevComponents.DotNetBar.eDotNetBarStyle.StyleManagerControlled;
+            this.bt_opensource.TabIndex = 9;
+            this.bt_opensource.Text = "浏览";
+            this.bt_opensource.Click += new System.EventHandler(this.bt_opensource_Click);
+            // 
+            // tb_source
+            // 
+            // 
+            // 
+            // 
+            this.tb_source.Border.Class = "TextBoxBorder";
+            this.tb_source.Border.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.tb_source.Location = new System.Drawing.Point(80, 13);
+            this.tb_source.Name = "tb_source";
+            this.tb_source.ReadOnly = true;
+            this.tb_source.Size = new System.Drawing.Size(434, 21);
+            this.tb_source.TabIndex = 8;
+            // 
+            // labelX1
+            // 
+            this.labelX1.BackColor = System.Drawing.Color.Transparent;
+            // 
+            // 
+            // 
+            this.labelX1.BackgroundStyle.Class = "";
+            this.labelX1.BackgroundStyle.CornerType = DevComponents.DotNetBar.eCornerType.Square;
+            this.labelX1.Location = new System.Drawing.Point(12, 13);
+            this.labelX1.Name = "labelX1";
+            this.labelX1.Size = new System.Drawing.Size(62, 23);
+            this.labelX1.TabIndex = 7;
+            this.labelX1.Text = "文件路径:";
+            // 
+            // Pro
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Menu;
+            this.ClientSize = new System.Drawing.Size(741, 487);
+            this.Controls.Add(this.groupPanel1);
+            this.Controls.Add(this.superTabControl1);
+            this.Name = "Pro";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "ProRob组学分析软件";
+            this.Load += new System.EventHandler(this.Pro_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.superTabControl1)).EndInit();
+            this.superTabControl1.ResumeLayout(false);
+            this.superTabControlPanel1.ResumeLayout(false);
+            this.groupPanel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private DevComponents.DotNetBar.TabItem tab_help;
+        private DevComponents.DotNetBar.TabItem tab_log;
+        private DevComponents.DotNetBar.SuperTabControl superTabControl1;
+        private DevComponents.DotNetBar.SuperTabControlPanel superTabControlPanel1;
+        private System.Windows.Forms.ListBox list_log;
+        private DevComponents.DotNetBar.SuperTabItem superTabItem1;
+        private DevComponents.DotNetBar.Controls.GroupPanel groupPanel1;
+        private DevComponents.DotNetBar.ButtonX bt_start;
+        private DevComponents.DotNetBar.ButtonX bt_opensource;
+        private DevComponents.DotNetBar.Controls.TextBoxX tb_source;
+        private DevComponents.DotNetBar.LabelX labelX1;
+    }
+}
+
---
+++ ProRob/source/ProRob/ProRob/Pro.cs
@@ -0,0 +1,228 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Xml;
+using System.Windows.Forms;
+using System.Threading;
+
+namespace ProRob
+{
+    public partial class Pro : Form
+    {
+        public string sourceFilePath;
+        public string modelFilePath;
+        public DataTable dtmod;
+        public int srcSheetNum = 2;
+
+        public Pro()
+        {
+            InitializeComponent();
+        }
+
+        public void init()
+        {
+            modelFilePath = getApplicationPath() + "/" + GetConfigValue("appSettings", "modFileName");
+            
+            try
+            {
+                string str_srcSheetNum = GetConfigValue("appSettings", "srcSheetNum");
+                srcSheetNum = Int32.Parse(str_srcSheetNum);
+            }
+            catch (Exception e) { }
+        }
+        public String getApplicationPath()
+        {
+            System.Reflection.Assembly curPath = System.Reflection.Assembly.GetExecutingAssembly();
+            String path = curPath.Location;
+            path = Path.GetDirectoryName(path);
+            return path;
+        }
+
+
+        public string GetOutFilePath(string path)
+        {
+            string parent = Path.GetDirectoryName(path);
+            string fileName = Path.GetFileNameWithoutExtension(path);
+            string addName = GetConfigValue("appSettings", "addName");
+            string extension = Path.GetExtension(modelFilePath);
+            return Path.Combine(parent, fileName + addName + extension);
+        }
+
+        public string GetConfigValue(string group, string appKey)
+        {
+            XmlDocument xDoc = new XmlDocument();
+            try
+            {
+                xDoc.Load(System.Windows.Forms.Application.ExecutablePath + ".config");
+                XmlNode xNode;
+                XmlElement xElem;
+                xNode = xDoc.SelectSingleNode("//" + group);
+                xElem = (XmlElement)xNode.SelectSingleNode("//add[@key='" + appKey + "']");
+                if (xElem != null)
+                    return xElem.GetAttribute("value");
+                else
+                    return "";
+            }
+            catch (Exception)
+            {
+                return "";
+            }
+        }
+
+        private delegate void Active();
+        /// <summary>
+        /// 应用程序的主入口点。
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Pro());
+        }
+
+        private void bt_opensource_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog F = new OpenFileDialog();
+            if (F.ShowDialog() == DialogResult.OK)
+            {
+                sourceFilePath = F.FileName;
+                tb_source.Text = sourceFilePath;
+            }
+        }
+
+        private void bt_start_Click(object sender, EventArgs e)
+        {
+
+            Thread start = new Thread(new ThreadStart(delegate() {
+
+                this.Invoke(new Active(delegate
+                {
+                    bt_start.Enabled = false;
+                    list_log.Items.Add(getTime()+"开始加载文件");
+                }));
+
+                DataTable dt_src;
+                try {
+                    dt_src = NpoiHelper.ImportExcel2007toDt(sourceFilePath, 2);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(sourceFilePath+"-->打开失败，请确认此文件没有被打开");
+                    this.Invoke(new Active(delegate
+                    {
+                        list_log.Items.Add(getTime() + "出现异常：" + e.ToString());
+                        list_log.Items.Add("");
+                        bt_start.Enabled = true;
+                    }));
+                    return;
+                }
+
+                if(dtmod == null)
+                    try
+                    {
+                        dtmod = NpoiHelper.ImportExcel2007toDt(modelFilePath);
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show(modelFilePath + "-->打开失败，请确认此文件没有被打开");
+                        this.Invoke(new Active(delegate
+                        {
+                            list_log.Items.Add(getTime() + "出现异常：" + e.ToString());
+                            list_log.Items.Add("");
+                            bt_start.Enabled = true;
+                        }));
+                        return;
+                    }
+                    
+
+                this.Invoke(new Active(delegate
+                {
+                    list_log.Items.Add(getTime() + "文件加载完毕");
+                    list_log.Items.Add(getTime() + "开始关联数据");
+                }));
+
+                if (dt_src.Columns.Count < 2)
+                {
+                    this.Invoke(new Active(delegate
+                    {
+                        MessageBox.Show(sourceFilePath + "-->分析失败，请输入正确格式的文件");
+                        list_log.Items.Add(getTime() + "请输入正确格式的文件");
+                        list_log.Items.Add("");
+                        bt_start.Enabled = true;
+                    }));
+                    return;
+                }
+                    
+
+                DataTable dt_mod = dtmod.Copy();
+                DataSet ds = new DataSet();
+
+                ds.Tables.Add(dt_mod);
+                ds.Tables.Add(dt_src);
+
+                ds.Relations.Add("dt", dt_mod.Columns[1], dt_src.Columns[1], false);
+                DataTable dt = ds.Relations["dt"].ParentTable;
+
+                int[][] data = new int[dt.Rows.Count+1][];
+                data[0] = new int[] { 0, 0 };
+                int m = 1;
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    DataRow[] drs = dt.Rows[i].GetChildRows("dt");
+                    if (drs.Length > 0)
+                    {
+                        data[m] = new int[2];
+                        data[m][0] = i + 1;
+                        data[m][1] = 0;
+                        m++;
+                    }
+                }
+                
+                this.Invoke(new Active(delegate
+                {
+                    list_log.Items.Add(getTime() + "关联数据完毕");
+                    list_log.Items.Add(getTime() + "载入文件总数：" + dt_src.Rows.Count + "; 匹配行数为:" + (m-1));
+                    list_log.Items.Add(getTime() + "开始写入文件");
+                }));
+
+                string outPath = GetOutFilePath(sourceFilePath);
+                try {
+                    NpoiHelper.DoWork(outPath, modelFilePath, data);
+                }
+                catch (Exception ex) {
+                    MessageBox.Show(outPath + "-->文件写入失败，请查看此目录是否具有写权限");
+                    this.Invoke(new Active(delegate
+                    {
+                        list_log.Items.Add(getTime() + "出现异常：" + e.ToString());
+                        list_log.Items.Add("");
+                        bt_start.Enabled = true;
+                    }));
+                    return;
+                }
+                
+                this.Invoke(new Active(delegate
+                {
+                    list_log.Items.Add(getTime() + "写入文件完毕"); 
+                    list_log.Items.Add("");
+                    bt_start.Enabled = true;
+                }));
+            }));
+
+            start.Start();
+        }
+
+        public string getTime()
+        {
+            return DateTime.Now.ToString("yyyy-MM-dd HH:mm:ss") + ":";
+        }
+
+        private void Pro_Load(object sender, EventArgs e)
+        {
+            init();
+        }
+    }
+}
